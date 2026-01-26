353d2c7a02f114a52ba84c632a307f34662a8748
VidhitaYadav <yadavvidhita@gmail.com>
2026-01-26 11:54:53 +0530

    Time: 1 ms (99.82%), Space: 47.3 MB (71.08%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int n=grid.length;
-        int N=n*n;
-        int repeated=-1,missing=-1;
-        int freq[]=new int[N+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                freq[grid[i][j]]++;
-            }
-        }
-        for(int i=1;i<=N;i++){
-            if(freq[i]==2){
-                repeated=i;
-            }else if(freq[i]==0){
-                missing=i;
-            }
-        }
-        return new int[]{repeated, missing};
-        
-        
+       long actualSum = 0, actualSquareSum = 0;
+           int n = grid.length;
+           long N = (long) n * n;
+           for (int i = 0; i < n; i++) {
+               for (int j = 0; j < n; j++) {
+                   int num = grid[i][j];
+                   actualSum += num;
+                   actualSquareSum += (long) num * num;
+               }
+           }
+           long expectedSum = (N * (N + 1)) / 2;
+           long expectedSquareSum = (N * (N + 1) * (2 * N + 1)) / 6;
+           long sumDifference = actualSum - expectedSum;
+           long squareSumDifference = actualSquareSum - expectedSquareSum;
+           long sumAb = squareSumDifference / sumDifference;
+           int repeated = (int) ((sumAb + sumDifference) / 2);
+           int missing = (int) ((sumAb - sumDifference) / 2);
+           return new int[]{repeated, missing};
+       }
     }
-}
