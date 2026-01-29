7032a569bcabcc6da79e18af0c383705aa90ac2d
VidhitaYadav <yadavvidhita@gmail.com>
2026-01-29 18:57:40 +0530

    Time: 1 ms (24.51%), Space: 43.7 MB (5.3%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        String str="";
+        for(int i=0;i<s.length();i++){
+            str=s.substring(i)+s.substring(0,i);
+            if(str.equals(goal)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
